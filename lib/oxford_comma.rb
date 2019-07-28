079c261b756fd8d3a2ba1dfe2851317801683f24
Jarred Wilkins <wilkins.jarred@outlook.com>
2019-07-28 20:14:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+require 'pry'
 
-end
+def oxford_comma(array)
+  
+  if array.length==1
+    array.join
+  elsif array.length==2 
+    array.join(" and ")
+  elsif str= array.pop
+    array.join(", ") <<", and #{str}"
+    
+  end  
+#binding.pry
+ end
